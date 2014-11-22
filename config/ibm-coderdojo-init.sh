7404aa827d66676770a0c201a70246655bbc6702
Lorenzo Cipriani <lorenzo1974@gmail.com>
2014-11-22 12:06:31 +0000

    Create ibm-coderdojo-init.sh

---
+++ config/ibm-coderdojo-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get -y update
+
+sudo apt-get -y dist-upgrade
+
+# TODO: config /etc/networks/interfaces
+
+sudo apt-get -y install vim
+
+sudo apt-get -y install tightvncserver
+
+sudo su -
+wget -O https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/config/vncserver > /etc/init.d/vncserver 
+chmod ugo+x /etc/init.d/vncserver
+update-rc.d vncserver defaults
+exit
