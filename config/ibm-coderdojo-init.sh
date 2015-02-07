150120b82688784ef852b8e174122f0f0809f664
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-02-07 06:27:52 +0000

    Update ibm-coderdojo-init.sh

--- config/ibm-coderdojo-init.sh
+++ config/ibm-coderdojo-init.sh
@@ -2,13 +2,22 @@
 
 sudo apt-get -y update
 
-sudo apt-get -y dist-upgrade
+sudo apt-get -y upgrade
 
 # TODO: config /etc/networks/interfaces
 
+echo -e "Install text editors"
 sudo apt-get -y install vim
 
 sudo apt-get -y install tightvncserver
+
+sudo apt-get -y install xrdp
+
+echo -e "Install development tools"
+sudo apt-get -y install -t stable subversion subversion-tools
+sudo apt-get -y install -t stable git git-doc git-cvs git-svn git-gui
+sudo apt-get -y install -t stable geany
+sudo apt-get -y install -t stable diffuse
 
 sudo su -
 wget -O /etc/init.d/vncserver https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/config/vncserver 
