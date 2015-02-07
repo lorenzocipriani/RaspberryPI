33df189d749fec4effa1473c68c33159ad3f543b
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-02-07 08:57:28 +0000

    Update ibm-coderdojo-init.sh

--- config/ibm-coderdojo-init.sh
+++ config/ibm-coderdojo-init.sh
@@ -3,14 +3,16 @@
 echo -e "Run the script as root"
 sudo su -
 
+echo -e "\n\nConfigure the nework"
+wget -O /etc/network/interfaces https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/etc/network/interfaces
+
 echo -e "\n\nUpdate the system to latest package releases"
 apt-get -y update
 apt-get -y dist-upgrade
 
-# TODO: config /etc/networks/interfaces
-
-echo -e "\n\nInstall text editors"
-apt-get -y install vim
+echo -e "\n\nInstall misc tools"
+apt-get -y install vim vim-doc vim-scripts
+apt-get -y vlc 
 
 echo -e "\n\nInstall remote desktop (VNC and RDP) servers"
 apt-get -y install tightvncserver
@@ -25,6 +27,9 @@ apt-get -y install -t stable subversion subversion-tools
 apt-get -y install -t stable git git-doc git-cvs git-svn git-gui
 apt-get -y install -t stable geany
 apt-get -y install -t stable diffuse
+
+echo -e "\n\nRestart the network"
+service networking restart
 
 echo -e "\n\nLeave the root session"
 exit
