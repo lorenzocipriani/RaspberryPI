b8c98ab692c80db72cb99881bd5409a7f17fd249
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-02-07 11:29:02 +0000

    Update ibm-coderdojo-init.sh

--- config/ibm-coderdojo-init.sh
+++ config/ibm-coderdojo-init.sh
@@ -8,19 +8,25 @@ wget -O /etc/network/interfaces https://raw.githubusercontent.com/lorenzociprian
 
 echo -e "\n\nUpdate the system to latest package releases"
 apt-get -y update
-apt-get -y dist-upgrade
+#apt-get -y dist-upgrade
 
 echo -e "\n\nInstall misc tools"
 apt-get -y install vim vim-doc vim-scripts
 apt-get -y vlc 
 
 echo -e "\n\nInstall remote desktop (VNC and RDP) servers"
-apt-get -y install tightvncserver
-apt-get -y install xrdp
+apt-get -y purge vnc4server
+apt-get -y purge tightvncserver
+apt-get -y purge xrdp
 
+apt-get -y install vnc4server
+apt-get -y install tightvncserver
 wget -O /etc/init.d/vncserver https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/etc/init.d/vncserver 
 chmod ugo+x /etc/init.d/vncserver
 update-rc.d vncserver defaults
+
+apt-get -y install xrdp
+
 
 echo -e "\n\nInstall development tools"
 apt-get -y install -t stable subversion subversion-tools
