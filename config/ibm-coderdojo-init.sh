504274ea4909f2f182c6dc268cec8c4eceab8b5a
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-03-21 13:18:09 +0000

    Update ibm-coderdojo-init.sh

--- config/ibm-coderdojo-init.sh
+++ config/ibm-coderdojo-init.sh
@@ -3,8 +3,21 @@
 echo -e "Run the script as root"
 sudo su -
 
-echo -e "\n\nConfigure the nework"
+echo -e "Configure pi default password"
+echo "pi:raspberry" | chpasswd
+
+echo -e "\n\nConfigure the network"
 wget -O /etc/network/interfaces https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/etc/network/interfaces
+if [ ! -f "/etc/ssh/sshd_config.orig" ]
+  cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+fi
+sed \
+  -e "s|#X11Forwarding|X11Forwarding/|" \
+  -e "s|X11Forwarding no|X11Forwarding yes/|" \
+  -e "s|#X11DisplayOffset|X11DisplayOffset/|" \
+  -e "s|#TCPKeepAlive|TCPKeepAlive/|" \
+  -e "s|TCPKeepAlive no|TCPKeepAlive yes/|" \
+  /etc/ssh/sshd_config.orig > /etc/ssh/sshd_config
 
 echo -e "\n\nUpdate the system to latest package releases"
 #apt-get -y update
@@ -12,6 +25,13 @@ echo -e "\n\nUpdate the system to latest package releases"
 
 echo -e "\n\nInstall small network infrastructure: DNS, DHCP, Router Advertisement and Network Boot"
 apt-get -y install dnsmasq
+wget -O /etc/dnsmasq.d/coderdojo.conf https://raw.githubusercontent.com/lorenzocipriani/RaspberryPI/master/etc/dnsmasq.d/coderdojo.conf
+if [ ! -f "/etc/dnsmasq.conf.orig" ]
+  cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+fi
+sed \
+  -e "s|#conf-dir|conf-dir/|" \
+  /etc/dnsmasq.conf.orig > /etc/dnsmasq.conf
 
 echo -e "\n\nInstall misc tools"
 apt-get -y install vim vim-doc vim-scripts
